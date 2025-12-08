34f5a1be7491ba16416df4bbbb374838c4ac9134
Alex <alexyakimenkojr@gmail.com>
2025-12-08 17:30:46 +0200

    feature/tabs: add router tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import '@/global.css';
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-slate-950">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
