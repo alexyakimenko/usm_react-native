2dc6098c01e1227e7245c6ffa25ca2caf69637dc
Alex <alexyakimenkojr@gmail.com>
2025-12-10 21:46:22 +0200

    feature/recipe: add recipe interface & schema fix

--- shared/api/db/schema/recipe.ts
+++ shared/api/db/schema/recipe.ts
@@ -3,13 +3,15 @@ import { category } from '@/shared/api/db/schema/category';
 
 export const recipe = sqliteTable('recipes', {
   id: int().primaryKey({ autoIncrement: true }),
-  categoryId: int('category_id').references(() => category.id),
+  categoryId: int('category_id')
+    .references(() => category.id)
+    .notNull(),
   title: text().notNull(),
   image: text(),
   ingredients: text().notNull(),
   description: text(),
   steps: text().notNull(),
   tags: text().notNull(),
-  likes: int().default(0),
-  dislikes: int().default(0),
+  likes: int().notNull().default(0),
+  dislikes: int().notNull().default(0),
 });
