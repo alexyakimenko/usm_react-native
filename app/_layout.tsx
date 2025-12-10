c3bc5f07aa2bd09e5e138f3a67d7bd780b0e25c1
Alex <alexyakimenkojr@gmail.com>
2025-12-10 21:48:18 +0200

    fix/styles: import global in top file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import MigrationProvider from '@/app/providers/MigrationProvider';
 import CategorySeedProvider from '@/app/providers/CategorySeedProvider';
 import { useColorScheme } from 'react-native';
+import '@/global.css';
 
 export default function RootLayout() {
   const colorscheme = useColorScheme();
